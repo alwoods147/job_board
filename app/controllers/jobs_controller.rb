810b256b8d79f34bb5438764353956bc2c8710e9
Alastair Woods <alastair@rosebankvilla.co.uk>
2016-11-26 14:32:40 +0000

    kjhkjb

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,6 @@
 class JobsController < ApplicationController
   def index
+    @jobs = Job.all
   end
   def new
   end
@@ -7,6 +8,25 @@ class JobsController < ApplicationController
   @job = Job.new
   end
   def create
+  	Job.create(job_params)
   	redirect_to jobs_path
   end
+  def destroy
+  @job = Job.find(params[:id])
+  @job.destroy
+  redirect_to jobs_path
+  end
+  def edit
+    @job = Job.find(params[:id])
+  end
+  private
+def update
+  @job = Job.find(params[:id])
+  @job.update_attributes(job_params)
+  redirect_to jobs_path
+end
+
+def job_params
+  params.require(:job).permit(:title, :description)
+end
 end
